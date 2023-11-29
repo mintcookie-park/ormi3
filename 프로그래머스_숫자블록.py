1aa18a797d8c27b4a99605b25538bc7f84f46a4f
Park-Jeonghun <79849531+mintcookie-park@users.noreply.github.com>
2023-11-29 16:05:13 +0900

    Add files via upload
    프로그래머스 멀리 뛰기, 숫자 블록 Python 문제 풀이입니다.

---
+++ 프로그래머스_숫자블록.py
@@ -0,0 +1,23 @@
+def get_max_divisor(n:int)->int:
+    if n==1:
+        return 0
+    
+    data=[]
+    
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            data.append(i)
+            if n//i<=10000000:
+                return n//i
+    if len(data)>=1:
+        return data[-1]
+    return 1
+
+def solution(begin, end):
+    answer = []
+    
+    for i in range(begin,end+1):
+        max_divisor=get_max_divisor(i)
+        answer.append(max_divisor)
+        
+    return answer
