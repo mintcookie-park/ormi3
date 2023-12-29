e5aff3b429eb7bd9916e00310e798194d4ce3495
Park-Jeonghun <79849531+mintcookie-park@users.noreply.github.com>
2023-12-29 16:53:31 +0900

    Add files via upload
    프로그래머스 기둥과 보 설치, 자물쇠와 열쇠 Python 문제풀이입니다.

---
+++ 프로그래머스_기둥과보설치.py
@@ -0,0 +1,26 @@
+def check(answer):
+    for x, y, stuff in answer:
+        if stuff == 0: #기둥 체크
+            #'바닥 위' or '보의 한쪽 끝 위' or '또 다른 기둥 위' 
+            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
+                continue
+            return False
+        elif stuff == 1: #보 체크
+            #'한쪽 끝 부분이 기둥 위' or '양쪽 끝 부분이 다른 보와 동시 연결'
+            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer):
+                continue
+            return False
+    return True
+
+def solution(n, build_frame):
+    answer = []
+    for build in build_frame:
+        x, y, stuff, operation = build
+        if operation == 1: # 설치
+            answer.append([x, y, stuff])
+            if not check(answer): answer.remove([x, y, stuff])
+        elif operation == 0: # 삭제
+            answer.remove([x, y, stuff])
+            if not check(answer): answer.append([x, y, stuff])
+    answer.sort()
+    return answer
