5af2165d89b9ec4d42473d7297ed4b6fd96c56a6
Park-Jeonghun <79849531+mintcookie-park@users.noreply.github.com>
2024-01-17 19:06:16 +0900

    Add files via upload
    프로그래머스 PCCE 기출문제 출력, 피타고라스의 정리 C++ 문제풀이입니다.

---
+++ 프로그래머스_출력.cpp
@@ -0,0 +1,36 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string msg = 
+"Spring is beginning"
+;
+    int val1 = 
+3
+;
+    string val2 = 
+"3"
+;
+
+    cout << msg << endl;
+    cout << val1 + 10 << endl;
+    cout << val2 + "10" << endl;
+
+    return 0;
+}
