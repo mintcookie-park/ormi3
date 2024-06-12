fc77cb445ebeecd12224b03c565342d9bfde5ec1
Park-Jeonghun <79849531+mintcookie-park@users.noreply.github.com>
2024-06-12 15:17:14 +0900

    Add files via upload
    프로그래머스 소수 찾기, 서울에서 김서방 찾기 Python 문제풀이입니다.

--- 프로그래머스_소수찾기.py
+++ 프로그래머스_소수찾기.py
@@ -1,22 +1,13 @@
-from itertools import permutations
+import math
+def isPrime(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
 
-def solution(numbers):
-    answer = []                                   
-    nums = [n for n in numbers]
-    per = []                                      
-    for i in range(1, len(numbers)+1):
-        per += list(permutations(nums, i))
-    new_nums = [int(("").join(p)) for p in per]
-
-    for n in new_nums:
-        if n < 2:
-            continue
-        check = True            
-        for i in range(2,int(n**0.5) + 1):
-            if n % i == 0:
-                check = False
-                break
-        if check:
-            answer.append(n)
-
-    return len(set(answer))
+def solution(n):
+    answer = 0
+    for i in range(2, n+1):
+        if isPrime(i) == True:
+            answer += 1
+    return answer
