59ad5823f34a7b5ea68821a08e4792d30ac2921c
Park-Jeonghun <79849531+mintcookie-park@users.noreply.github.com>
2024-10-16 23:21:33 +0900

    Add files via upload
    백준 11659 Python 문제풀이입니다.

---
+++ 백준_11659.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+sum = [0]
+tmp = 0
+
+for i in numbers:
+    tmp = tmp + i
+    sum.append(tmp)
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    print(sum[j] - sum[i-1])
