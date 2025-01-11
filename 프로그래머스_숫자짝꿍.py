22dcbd18703b5b8fe3fc4d504d94ae7aa3fbe9f8
Park-Jeonghun <79849531+mintcookie-park@users.noreply.github.com>
2025-01-11 21:43:01 +0900

    Add files via upload
    프로그래머스 숫자 짝꿍 Python 문제풀이입니다.

--- 프로그래머스_숫자짝꿍.py
+++ 프로그래머스_숫자짝꿍.py
@@ -1,8 +1,8 @@
 def solution(X, Y):
     answer = ''
 
-    for i in range(9, -1, -1):
-        answer += (str(i) * min(X.count(str(i)), Y.count(str(i))))
+    for i in range(9,-1,-1):
+        answer = (str(i)*min(X.count(str(i)), Y.count(str(i))))
     if answer == '':
         return '-1'
     elif len(answer) == answer.count('0'):
